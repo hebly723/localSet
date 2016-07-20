50f4fe2b3fbeee33079632d3d7440c3b3bd3d846
hebly723 <hebly723@163.com>
2016-07-20 20:08:28 +0800

    改与删

--- FirstMybatis/src/cn/itcast/mybatis/first/MybatisFirst.java
+++ FirstMybatis/src/cn/itcast/mybatis/first/MybatisFirst.java
@@ -73,4 +73,40 @@ public class MybatisFirst {
 		
 		sqlSession.close();
 	}
+	
+	@Test
+	public void insertUserTest() throws IOException
+	{
+		//mybatis配置文件
+		String resource = "SqlMapConfig.xml";
+		//得到配置文件流
+		InputStream inputStream=Resources.getResourceAsStream(resource);
+		//创建会话工厂，传入mybatis的配置文件信息
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().
+				build(inputStream);
+		//通过工厂得到SqlSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		City city = new City();
+		city.setCountryCode("GBT43243214");
+		city.setName("One4321412");
+		city.setPopulation(234);
+		city.setDistrict("QEW1432143214");
+		//通过SqlSession操作数据库
+		//第一个参数：映射文件中statement的ID，等于namespace+"."+statement的id
+		//第二个参数：指定和映射文件中所匹配的parameterType类型的参数
+		//sqlSession.selectOne结果是与 映射文件中所匹配的resultType类型的对象
+		/*List<City> list = */
+//		尝试用trycatch捕捉异常，这样就能知道insert是否成功
+//		try{
+		sqlSession.insert("test.insertCity", city);
+//		}catch(Exception e)
+//		{
+//			System.out.println("Wrong");
+//		}
+		System.out.println(city.getID());
+		//提交事务
+		sqlSession.commit();
+		
+		sqlSession.close();
+	}
 }
